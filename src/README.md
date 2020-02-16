f8357eaedea1a0609f9a2a316818abda8bfe020e
Qizhe Xie <qizhex@google.com>
2020-02-16 21:35:27 +0000

    init

---
+++ src/README.md
@@ -0,0 +1,58 @@
+# Noisy Student
+
+## Overview
+
+NoisyStudent is a semi-supervised learning method which achieves
+state-of-the-art results on ImageNet.
+
+For a detailed description of technical details and experimental results, please
+refer to our paper:
+
+[Self-training with Noisy Student improves ImageNet classification.](https://arxiv.org/abs/1911.04252)
+
+Qizhe Xie, Minh-Thang Luong, Eduard Hovy and Quoc V. Le.
+
+## Release Notes
+
+*   Feb 16, 2020: Code on SVHN.
+
+## SVHN experiments
+
+## Relevant Papers for Citation
+
+Noisy Student:
+
+```
+@article{xie2019self,
+  title={Self-training with Noisy Student improves ImageNet classification},
+  author={Xie, Qizhe and Hovy, Eduard and Luong, Minh-Thang and Le, Quoc V},
+  journal={arXiv preprint arXiv:1911.04252},
+  year={2019}
+}
+```
+
+EfficientNet:
+
+```
+@article{tan2019efficientnet,
+  title={Efficientnet: Rethinking model scaling for convolutional neural networks},
+  author={Tan, Mingxing and Le, Quoc V},
+  journal={arXiv preprint arXiv:1905.11946},
+  year={2019}
+}
+```
+
+RandAugment.
+
+```
+@article{cubuk2019randaugment,
+  title={RandAugment: Practical data augmentation with no separate search},
+  author={Cubuk, Ekin D and Zoph, Barret and Shlens, Jonathon and Le, Quoc V},
+  journal={arXiv preprint arXiv:1909.13719},
+  year={2019}
+}
+```
+
+## Disclaimer
+
+This is not an officially supported Google product.
---
+++ src/local_scripts/train.sh
@@ -0,0 +1,43 @@
+# coding=utf-8
+# Copyright 2019 The Google NoisyStudent Team Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#!/bin/bash
+
+dir_cell=yo
+model_dir=./ckpt/exp_1
+rm -r ${model_dir}
+
+task_name=svhn
+label_data_dir=./data/proc
+unlabel_data_dir=${label_data_dir}/unlabeled
+
+python main.py \
+    --model_name=efficientnet-b0 \
+    --use_tpu=False \
+    --model_dir=$model_dir \
+    --mode=train \
+    --train_batch_size=32 \
+    --iterations_per_loop=1 \
+    --save_checkpoints_steps=5 \
+    --use_bfloat16=True \
+    --label_data_dir=${label_data_dir} \
+    --input_image_size=32 \
+    --task_name=${task_name} \
+    --augment_name=randaug \
+    --unlabel_ratio=1 \
+    --teacher_softmax_temp=1 \
+    --teacher_model_name=efficientnet-b0 \
+    --use_bfloat16=False \
+    --teacher_model_path=/usr/local/google/home/qizhex/workspace/noisy_student/noisy_student_test/ckpt/teacher/model.ckpt-109474 \
+    --unlabel_data_dir=${unlabel_data_dir}

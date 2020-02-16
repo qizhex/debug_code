90aac9adcf87d8e808427be32000d4d040d2c069
Qizhe Xie <qizhex@google.com>
2020-02-16 22:30:25 +0000

    update

--- src/task_info.py
+++ src/task_info.py
@@ -17,6 +17,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import tensorflow as tf
 
 def get_mean_std(task_name):
   '''Get mean and std.'''
